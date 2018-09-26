606dd43c6578e5e120cab0fd4d569f28f497d3c7
Hannah Wolfe <erisds@gmail.com>
2018-09-26 20:15:48 +0100

    Trying to make it faster

--- packages/slimer/lib/new-project.js
+++ packages/slimer/lib/new-project.js
@@ -3,15 +3,34 @@
 const yoEnv = require('yeoman-environment');
 const env = yoEnv.createEnv();
 const debug = require('debug')('slimer');
+const path = require('path');
+
+
+const findSlimer = (callback) => {
+    let searchPath = path.resolve(__dirname, '../../');
+    let slimerPath = env.findGeneratorsIn([searchPath]);
+    let slimerPath = false;
+    let slimer;
+
+    if (slimerPath && slimerPath[0]) {
+        let ns = env.getByPath(path.join(slimerPath[0], 'generators', 'app'));
+        slimer = env.instantiate(ns, {options: {help: true}});
+        debug('found');
+        return callback(slimer);
+    } else {
+        env.lookup(() => {
+            debug('loaded');
+            slimer = env.create('@tryghost/slimer', {options: {help: true}});
+            debug('found');
+            return callback(slimer);
+        });
+    }
+};
 
 module.exports = (...args) => {
     debug('new project', ...args);
-    debug('Will look for generators in', env.getNpmPaths());
-    debug('Current path', __dirname);
 
-    env.lookup(() => {
-        debug('loaded', env.getGeneratorsMeta());
-        const slimer = env.create('slimer', {options: {help: true}});
+    findSlimer((slimer) => {
         slimer.run();
     });
 };
